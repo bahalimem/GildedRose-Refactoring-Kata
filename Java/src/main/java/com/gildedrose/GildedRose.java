d35b70eae0182ec06a0d00ce58e1a5bebe526a6d
bahalimem <bahaeddine.limem.pro@gmail.com>
2024-04-02 02:29:33 +0200

    improve code (test)

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,97 @@
 package com.gildedrose;
 
-class GildedRose {
-    Item[] items;
+public class GildedRose {
+    private Item[] items;
 
+    // Constructeur pour initialiser le tableau d'items
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
+    // Méthode pour mettre à jour la qualité de tous les items
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        // Parcourir chaque item dans le tableau items
+        for (Item item : items) {
+            updateItemQuality(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+    // Méthode pour mettre à jour la qualité d'un seul item
+    private void updateItemQuality(Item item) {
+        // Vérifier le type d'item et mettre à jour sa qualité en conséquence
+        if (isNormalItem(item)) {
+            decreaseQuality(item);
+        } else if (isAgedBrie(item)) {
+            increaseQuality(item);
+        } else if (isBackstagePass(item)) {
+            updateBackstagePass(item);
+        }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+        // Diminuer sellIn pour tous les items sauf Sulfuras
+        if (!isSulfuras(item)) {
+            item.setSellIn(item.getSellIn() - 1);
+        }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        // Mettre à jour la qualité si sellIn est passé
+        if (item.getSellIn() < 0) {
+            if (isNormalItem(item)) {
+                decreaseQuality(item);
+            } else if (isAgedBrie(item)) {
+                increaseQuality(item);
+            } else if (isBackstagePass(item)) {
+                item.setQuality(0);
             }
         }
     }
+
+    // Méthode d'aide pour vérifier si un item est un item normal
+    private boolean isNormalItem(Item item) {
+        return !isAgedBrie(item) && !isBackstagePass(item) && !isSulfuras(item);
+    }
+
+    // Méthode d'aide pour vérifier si un item est Aged Brie
+    private boolean isAgedBrie(Item item) {
+        return item.getName().equals("Aged Brie");
+    }
+
+    // Méthode d'aide pour vérifier si un item est Backstage Pass
+    private boolean isBackstagePass(Item item) {
+        return item.getName().equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    // Méthode d'aide pour vérifier si un item est Sulfuras
+    private boolean isSulfuras(Item item) {
+        return item.getName().equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    // Méthode d'aide pour augmenter la qualité d'un item
+    private void increaseQuality(Item item) {
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+        }
+    }
+
+    // Méthode d'aide pour diminuer la qualité d'un item
+    private void decreaseQuality(Item item) {
+        if (item.getQuality() > 0) {
+            item.setQuality(item.getQuality() - 1);
+        }
+    }
+
+    // Méthode d'aide pour mettre à jour la qualité pour Backstage Pass
+    private void updateBackstagePass(Item item) {
+        if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
+            if (item.getSellIn() < 11 && item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+            }
+            if (item.getSellIn() < 6 && item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+            }
+        }
+    }
+    public Item[] getItems() {
+        return items;
+    }
 }
+
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,12 +1,12 @@
 package com.gildedrose;
 
+import java.util.Objects;
+
 public class Item {
 
-    public String name;
-
-    public int sellIn;
-
-    public int quality;
+    private final String name;
+    private int sellIn;
+    private int quality;
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -14,8 +14,48 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Item item = (Item) obj;
+        return sellIn == item.sellIn &&
+            quality == item.quality &&
+            Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sellIn, quality);
     }
 }
